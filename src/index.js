eb8236aed451770710371e2c99dfdb8c0a7f2d72
baocter2004 <be93700@gmail.com>
2026-04-28 17:19:17 +0700

    Nodejs Template Engine - Handlebars

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// HTTP logger middleware
+app.use(morgan("combined"));
+
+// Set up Handlebars as the view engine, 
+// so that we can render .hbs files from the views directory
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  }),
+);
+// Set Handlebars as the view engine
+app.set("view engine", "hbs");
+// Set the directory for views
+app.set("views", path.join(__dirname, "resources\\views"));
+
+// Define a route for the root URL ("/")
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// Define a route for the news URL ("/news")
+app.get("/news", (req, res) => {
+  res.render("tintuc");
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
